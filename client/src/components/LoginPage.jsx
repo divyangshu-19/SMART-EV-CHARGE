9983fec650ccf6f9130be834c9797d0e233443dc
soumyajitg16 <soumyajitg16@gmail.com>
2024-06-02 19:59:17 +0530

    Google button updated in LoginPage.jsx and .css

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -141,12 +141,17 @@ function LoginPage({ onLogin, role }) {
                 )}
                 <button type="submit" disabled={isSigningIn} className="btn btn-primary">Login</button>
               </form>
-              <span className="form-text">Don't have an account? <button onClick={() => setIsRegistering(true)} className="btn btn-link">Register</button></span>
+              <span className="form-text">Don't have an account? <button onClick={() => setIsRegistering(true)} className="btn-register">Register</button></span>
+              <div className="divider">
+                <div className="divider-line"></div>
+                <div className="divider-text">OR</div>
+                <div className="divider-line"></div>
+              </div>
               <button
-                onClick={onGoogleSignIn}
-                className={`w-full flex items-center justify-center gap-x-3 py-2.5 border rounded-lg text-sm font-medium ${isSigningIn ? 'cursor-not-allowed' : 'hover:bg-gray-100 transition duration-300 active:bg-gray-100'}`}
+                onClick ={(e) => {onGoogleSignIn(e)}}
+                className={`btn-google ${isSigningIn ? 'disabled' : ''}`}
                 disabled={isSigningIn}>
-                <svg className="w-5 h-5" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className="google-icon" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <g clipPath="url(#clip0_17_40)">
                     <path d="M47.532 24.5528C47.532 22.9214 47.3997 21.2811 47.1175 19.6761H24.48V28.9181H37.4434C36.9055 31.8988 35.177 34.5356 32.6461 36.2111V42.2078H40.3801C44.9217 38.0278 47.532 31.8547 47.532 24.5528Z" fill="#4285F4" />
                     <path d="M24.48 48.0016C30.9529 48.0016 36.4116 45.8764 40.3888 42.2078L32.6549 36.2111C30.5031 37.675 27.7252 38.5039 24.4888 38.5039C18.2275 38.5039 12.9187 34.2798 11.0139 28.6006H3.03296V34.7825C7.10718 42.8868 15.4056 48.0016 24.48 48.0016Z" fill="#34A853" />
